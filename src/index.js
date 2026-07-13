f169635d0ae0dcdee1e578bf5dae8beca9ea3c7f
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-13 20:47:46 +0500

    create the todo class for creating todos

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// TO Do list using oop principles.
+
+//class to create reminders
+class todos
+{
+    constructor(title,description,due,priority,notes)
+    {
+        this.title= title;
+        this.description=description;
+        this.due=due;
+        this,priority=priority;
+        this.notes=notes;
+    }
+}
