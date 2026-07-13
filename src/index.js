b0e555546772fb215c82cf69572e4e18f9475110
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-13 21:05:24 +0500

    create a class to log information and errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // TO Do list using oop principles.
 
+//class to run the app in console
+class consoleMenu
+{
+
+};
 //class to create reminders
 class todos
 {
@@ -10,5 +15,17 @@ class todos
         this.due=due;
         this,priority=priority;
         this.notes=notes;
+        logger.info(`toDo Created with title {${this.title}} constructor definition at line 11`);
     }
 }
+//class to log messages or errors to the console
+class logger
+{
+    //this class is a percfect example of why sometimes you don't need a constructor at all 
+    info(message)
+    {
+        console.log(`[Info] ${message}`);
+    }
+
+}
+const logix = new logger();
