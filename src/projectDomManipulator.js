50443357382e6caaeecd9bb8287f55ba92d039b4
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-16 07:46:50 +0500

    initialize a single eventlistner for modal buttons Previously separate eventlistners were used for both buttons. now same eventlistner is used which then separates button logic by calling their respective handlers

--- src/projectDomManipulator.js
+++ src/projectDomManipulator.js
@@ -1,12 +1,28 @@
 import Projects from './project.js'
 export default class domManipulator
 {
-    #count = 0;
+    #count=0;
     constructor(main)
     {
         console.log('Dom Object succesfully created');
         this.runner = main;
-        let projectList = document.querySelector('.projectList');
+        this.projectList = document.querySelector('.projectList');
+        this.modal = document.querySelector('.modalOverlay');
+        let createOptions = document.querySelector('.creatorOptions');
+        createOptions.addEventListener('click',(e)=>
+        {
+            let target = e.target.id;
+            if(target==='cancel')
+            {
+                this.cancelButton()
+            }
+        })
+        
+        
+    }
+    cancelButton()
+    {
+            this.modal.classList.add('hidden');
     }
     initializeProjectCreation()
     {
@@ -15,8 +31,9 @@ export default class domManipulator
         {
             // this.runner.addProjectToArray(new Projects('Default'));
             let projectDiv = document.createElement('div');
-            count++;
-            projectDiv.id=`project_${count}`;
+            this.#count++;
+            projectDiv.id=`project_${this.#count}`;
+            this.modal.classList.remove('hidden');
 
         })
     }
