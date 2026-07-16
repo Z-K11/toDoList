65728c64639b810610f302fe2946318b96809366
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-07-16 06:30:23 +0500

    create modal overlay menu for project creation dialogue

--- src/projectDomManipulator.js
+++ src/projectDomManipulator.js
@@ -1,10 +1,12 @@
 import Projects from './project.js'
 export default class domManipulator
 {
+    #count = 0;
     constructor(main)
     {
         console.log('Dom Object succesfully created');
         this.runner = main;
+        let projectList = document.querySelector('.projectList');
     }
     initializeProjectCreation()
     {
@@ -12,6 +14,10 @@ export default class domManipulator
         creatorButton.addEventListener('click',()=>
         {
             // this.runner.addProjectToArray(new Projects('Default'));
+            let projectDiv = document.createElement('div');
+            count++;
+            projectDiv.id=`project_${count}`;
+
         })
     }
 }
